6f9eb1440cfd00392e26ab0141beef21d36f1f70
alex <cyy0523xc@gmail.com>
2018-07-07 10:53:25 +0800

    *

--- merge.sh
+++ merge.sh
@@ -5,16 +5,15 @@
 # Created Time: 2018年07月04日 星期三 16时33分04秒
     #./format_datasets/weibo_senti_100k/weibo_senti_100k_neg.txt \
     #./format_datasets/weibo_senti_100k/weibo_senti_100k_pos.txt \
+    #./format_datasets/waimai_10k/waimai_10_neg.txt \
 
 cat ./format_datasets/base/neg.txt \
     ./format_datasets/online_shopping_10_cats/online_shopping_10_cats_neg.txt \
-    ./format_datasets/waimai_10k/waimai_10_neg.txt \
 > tmp.txt
 cat tmp.txt|sort|uniq > ./format_datasets/total_neg.txt
 
 cat ./format_datasets/base/pos.txt \
     ./format_datasets/online_shopping_10_cats/online_shopping_10_cats_pos.txt \
-    ./format_datasets/waimai_10k/waimai_10_pos.txt \
 > tmp.txt
 cat tmp.txt|sort|uniq > ./format_datasets/total_pos.txt
 
